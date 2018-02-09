201675b516fbd479a92e00ed14bfa56b44d990c2
Erika Balbino <erk.balbino@gmail.com>
2018-02-08 16:38:00 -0800

    Created the views to show the index and the cohorts

--- homework-4/super_team_picker/routes/teams.js
+++ homework-4/super_team_picker/routes/teams.js
@@ -5,35 +5,75 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', (req, res, next) => {
 
-  res.render('teams/index');
+  res.render('teams/new');
 
 });
 
 router.post('/', (req, res) => {
-  const logoUrl = req.body.logoUrl;
-  const name = req.body.name;
-  const members = req.body.members;
+  if (req.body.logUrl || req.body.name || req.body.members) {
+    const logoUrl = req.body.logoUrl;
+    const name = req.body.name;
+    const members = req.body.members;
 
-  knex
-    .insert({
-      logoUrl: logoUrl,
-      name: name,
-      members: members,
-    })
-    .into('cohorts')
-    .then(() => {
-      res.redirect('/teams');
-    });
+    knex
+      .insert({
+        logoUrl: logoUrl,
+        name: name,
+        members: members,
+      })
+      .returning('id')
+      .into('cohorts')
+      .then((id) => {
+        //res.redirect('/teams/index');
+        res.redirect(`/teams/${id}`);
+      });
+  } else {
+    res.render('teams/new');
+  }
 });
 
-router.get('/show', (req, res) => {
+// router.post('/:id', (req, res) => {
+//   const id = req.params.id;
+//   res.redirect(`/teams/${id}`);
+//
+// });
+
+router.get('/index', (req, res) => {
+  // if (req.body.logUrl || req.body.name || req.body.members) {
+  // } else {
+  //   res.render('teams/index');
+  // }
   knex
-    .select()
-    .from('cohorts')
-    .then(cohorts => {
-      console.log(cohorts);
-      res.render('teams/show', {cohorts: cohorts});
-    });
+  .select()
+  .from('cohorts')
+  .then(cohorts => {
+    console.log(cohorts);
+    res.render('teams/index', { cohorts: cohorts });
+  });
+
+});
+
+router.get('/:id', (req, res) => {
+  // Paths that have `:` in their name will ne stored as a key value
+  // in req.params. Use it to the `id` of a post.
+  console.log(req.params);
+  const teamId = req.params.id;
+
+  if (isNaN(parseInt(teamId, 10))) {
+    return res.redirect('/');
+  }
+
+  knex
+      // .select()
+      .first() // replace select with first when you only want one row
+      .from('cohorts')
+      .where({id: teamId})
+      .then(cohorts => {
+        res.render('teams/show', {cohorts: cohorts || {}});
+        // res.send(cohorts); --> so para testar
+      })
+
+    // res.send(req.params) --> so para testar
 });
 
 module.exports = router;
