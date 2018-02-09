a259dd81e6c6a8d92b53540177551bc1e655fdf2
Erika Balbino <erk.balbino@gmail.com>
2018-02-08 19:38:43 -0800

    Implemented the functionality to generate the teams and display the show form

--- homework-4/super_team_picker/routes/teams.js
+++ homework-4/super_team_picker/routes/teams.js
@@ -56,24 +56,85 @@ router.get('/index', (req, res) => {
 router.get('/:id', (req, res) => {
   // Paths that have `:` in their name will ne stored as a key value
   // in req.params. Use it to the `id` of a post.
-  console.log(req.params);
+  // console.log(req.params);
   const teamId = req.params.id;
+  const quantity = Number(req.query.quantity);
+  const opt = req.query.method;
 
   if (isNaN(parseInt(teamId, 10))) {
     return res.redirect('/');
   }
 
   knex
-      // .select()
-      .first() // replace select with first when you only want one row
-      .from('cohorts')
-      .where({id: teamId})
-      .then(cohorts => {
-        res.render('teams/show', {cohorts: cohorts || {}});
-        // res.send(cohorts); --> so para testar
-      })
+    // .select()
+    .first() // replace select with first when you only want one row
+    .from('cohorts')
+    .where({id: teamId})
+    .then(cohorts => {
+      // console.log('co', typeof cohorts.members);
+      if (isNaN(parseInt(quantity)) || opt === undefined || cohorts.members === []) {
+        return res.render('teams/show', {
+          cohorts: cohorts || {}, group: [], group2: {}
+        });
+      }
+      // console.log('test');
+      let quan_arr = cohorts.members.split(',').map(name => name.trim());
+      let person = Math.floor(quan_arr.length / quantity);
+      let person2 = Math.round(quan_arr.length / quantity);
+      const teams = {};
 
-    // res.send(req.params) --> so para testar
+      let quan = quantity;
+
+      let arr = [];
+      let num = quan_arr.length;
+
+      if (opt === 'teamCount') {
+        for (let i = 0; i < quantity; i++) {
+          console.log('teamCount');
+          arr[i] = [];
+          for (let j = 0; j < person; j++) {
+            let idx = Math.floor(Math.random() * quan_arr.length)
+            arr[i].push(quan_arr[idx]);
+            quan_arr.splice(idx, 1);
+          }
+        }
+        // }
+        // quan_arr only has people without groups
+        let rest = quan_arr.length
+        if (rest !== 0) {
+          for (let k = 0; k < rest; k++) {
+            let idx2 = Math.floor(Math.random() * quan_arr.length)
+            arr[k].push(quan_arr[idx2]);
+            quan_arr.splice(idx2, 1);
+          }
+        }
+      } else {
+        console.log('perTeam');
+        for (let l = 1; l <= person2; l++) {
+          teams[l] = [];
+        }
+        while (quan_arr.length > 0) {
+          for (let team in teams) {
+            let idx = Math.floor(Math.random() * quan_arr.length)
+            let spl = quan_arr.splice(idx, 1);
+            teams[team].push(spl[0]);
+            if (quan_arr.length === 0) {
+              break;
+            }
+          }
+        }
+      }
+      res.render('teams/show', {
+        cohorts: cohorts || {},
+        group: arr || [],
+        group2: teams || {}
+      });
+
+      // res.render('teams/show', {cohorts: cohorts || {}});
+      // res.send(cohorts); --> so para testar
+    })
+
+  // res.send(req.params) --> so para testar
 });
 
 module.exports = router;
