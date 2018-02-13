414bee48519504ccfc21b6acba0c2393ac42c10b
Erika Balbino <erk.balbino@gmail.com>
2018-02-12 18:23:04 -0800

    Add all CRUD for Posts

--- homework-5/blog_on_rails/config/routes.rb
+++ homework-5/blog_on_rails/config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # RESTful Question Routes
+  # All CRUD operations
+
+  # Post#new
+  get('/posts/new', to: 'posts#new', as: :new_post)
+
+  # Post#create
+  post('/posts', to: 'posts#create', as: :posts)
+
+  # Post#index
+  get('/posts', to: 'posts#index')
+
+  # Post#show
+  get('/posts/:id', to: 'posts#show', as: :post)
+
+  # Post#edit
+  get('/posts/:id/edit', to: 'posts#edit', as: :edit_post)
+
+  # Post#update
+  patch('/posts/:id', to: 'posts#update')
+  put('/posts/:id', to: 'posts#update')
+
+  # Post#destroy
+  delete('/posts/:id', to: 'posts#destroy')
+
 end
