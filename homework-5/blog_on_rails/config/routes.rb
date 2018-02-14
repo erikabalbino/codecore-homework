2956b4786a21e033e5d8d56010c9a950cb45d18e
Erika Balbino <erk.balbino@gmail.com>
2018-02-14 08:52:36 -0800

    Create comments model associated to posts and some styles

--- homework-5/blog_on_rails/config/routes.rb
+++ homework-5/blog_on_rails/config/routes.rb
@@ -1,27 +1,35 @@
 Rails.application.routes.draw do
-  # RESTful Question Routes
+
+  resources :posts do
+    resources :comments, only: [:create, :destroy], shallow: true
+    # The shallow argument will cause nested resource routes
+    # to only be prefixed by the parent resource if it is
+    # required. It's required for :index, :create
+  end
+  
+  # RESTful Post Routes
   # All CRUD operations
 
-  # Post#new
-  get('/posts/new', to: 'posts#new', as: :new_post)
-
-  # Post#create
-  post('/posts', to: 'posts#create', as: :posts)
-
-  # Post#index
-  get('/posts', to: 'posts#index')
-
-  # Post#show
-  get('/posts/:id', to: 'posts#show', as: :post)
-
-  # Post#edit
-  get('/posts/:id/edit', to: 'posts#edit', as: :edit_post)
-
-  # Post#update
-  patch('/posts/:id', to: 'posts#update')
-  put('/posts/:id', to: 'posts#update')
-
-  # Post#destroy
-  delete('/posts/:id', to: 'posts#destroy', as: :delete_post)
+  # # Post#new
+  # get('/posts/new', to: 'posts#new', as: :new_post)
+  #
+  # # Post#create
+  # post('/posts', to: 'posts#create', as: :posts)
+  #
+  # # Post#index
+  # get('/posts', to: 'posts#index')
+  #
+  # # Post#show
+  # get('/posts/:id', to: 'posts#show', as: :post)
+  #
+  # # Post#edit
+  # get('/posts/:id/edit', to: 'posts#edit', as: :edit_post)
+  #
+  # # Post#update
+  # patch('/posts/:id', to: 'posts#update')
+  # put('/posts/:id', to: 'posts#update')
+  #
+  # # Post#destroy
+  # delete('/posts/:id', to: 'posts#destroy', as: :delete_post)
 
 end
--- homework-5/blog_on_rails/db/seeds.rb
+++ homework-5/blog_on_rails/db/seeds.rb
@@ -2,12 +2,22 @@
 Post.destroy_all
 
 50.times.each do
-  Post.create(
+  p = Post.create(
     title: Faker::Job.title,
-    body: Faker::Lorem.paragraph,
+    body: Faker::Lorem.paragraph
   )
+  if p.valid?
+    rand(0..10).times.each do
+      Comment.create(
+        body: Faker::Job.field,
+        post: p
+      )
+    end
+  end
 end
 
 posts=Post.all
+comments = Comment.all
 
 puts Cowsay.say "Created #{posts.count} posts", :frogs
+puts Cowsay.say "Created #{comments.count} comments", :sheep
