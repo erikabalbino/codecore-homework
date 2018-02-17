2449db7ad7be3e3b6e815ff0337801188076d011
Erika Balbino <erk.balbino@gmail.com>
2018-02-16 18:22:29 -0800

    Initial commit

---
+++ homework-6/blog_on_rails/app/models/post.rb
@@ -0,0 +1,19 @@
+class Post < ApplicationRecord
+
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true, uniqueness: true
+  validates :body,
+    presence: {message: "must be given"},
+    length: {minimum: 50}
+
+  #custom validate:
+  validate :no_monkey
+
+  private
+  def no_monkey
+    if body&.downcase.include?('monkey')
+      errors.add(:body,  "must not have a monkey")
+    end
+  end
+end
